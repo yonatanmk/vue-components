9a1b87eb8f7164606ae3ad49844d5d1febf01043
yonatanmk <yonatanmk@gmail.com>
2023-04-27 17:23:23 -0700

    home page nav styles

--- src/layouts/HomeLayout/HomeLayout.vue
+++ src/layouts/HomeLayout/HomeLayout.vue
@@ -1,9 +1,14 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/table">Table</router-link> |
-  </nav>
-  <!-- <router-view/> -->
+  </nav> -->
+  <div className="App">
+    <nav className="App__Container">
+      <router-link className="App__Link" to="/">Home</router-link>
+      <router-link className="App__Link" to="/table">Table</router-link>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss">
@@ -15,16 +20,36 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.App {
+  padding: 2rem;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  &__Container {
+    max-width: 60rem;
+    margin: auto;
+    display: flex;
+    // padding: 2rem;
+    border: 1px solid black;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  &__Link {
+    // background-color: red;
+    padding: 1rem;
+    text-align: center;
+    text-decoration: none;
+    color: black;
+
+    &:not(:first-child) {
+      border-top: 1px solid #CCC;
+    }
+
+    &:hover {
+      background-color: #CCC;
     }
   }
 }
+
 </style>
